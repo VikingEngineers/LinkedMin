682582bec69be0f069de43efd3602f82f6bcc74e
lazyseal <fearmer@gmail.com>
2023-05-04 12:38:02 +0000

    Requirements updated, run server script created

--- runmyserver.sh
+++ runmyserver.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
-echo "Running server"
+#Use command: bash ronmyserver.sh
+echo "Running WEB-server with default settings"
 source /home/pupa/somebodyhire/venv/bin/activate
 python3 /home/pupa/LinkedMin/linkedmin/manage.py runserver 0.0.0.0:8000
