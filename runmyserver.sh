c320843756da4a750e551e8710b76339d6d6ada2
lazyseal <49525095+lazyseal@users.noreply.github.com>
2023-05-04 13:41:21 +0100

    Update runmyserver.sh

--- runmyserver.sh
+++ runmyserver.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-#Use command: bash ronmyserver.sh
+#Use command: bash runmyserver.sh or ./runmyserver.sh
 echo "Running WEB-server with default settings"
 source /home/pupa/somebodyhire/venv/bin/activate
 python3 /home/pupa/LinkedMin/linkedmin/manage.py runserver 0.0.0.0:8000
