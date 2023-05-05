52a30f0ef1d26e6c21e779eb773e5aa8f7cae211
kateschka <sova.krs@gmail.com>
2023-05-05 17:45:13 +0300

    added emails for sign up and password reset

--- linkedmin/users/signals.py
+++ linkedmin/users/signals.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 # create user profile automatically when user is created
 #@receiver(post_save, sender=Profile)    
@@ -14,6 +17,17 @@ def createProfile(sender, instance, created, **kwargs):
             username = user.username,
             email = user.email,
             name = user.first_name,
+        )
+
+        subject = 'Welcome to LinkedMin'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 def updateUser(sender, instance, created, **kwargs):
