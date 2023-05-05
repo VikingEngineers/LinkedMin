d8adcbe4e818849a2eee644b4943a577cfe62816
kateschka <sova.krs@gmail.com>
2023-05-05 20:35:42 +0300

    fixed api view, added imports

--- linkedmin/api/views.py
+++ linkedmin/api/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Tag, Review
 
 
 @api_view(['GET'])
